f3a09513c4305f67ec4181193ea6a97ae1356767
sanam <simian114@stunning.kr>
2023-04-02 00:45:24 +0900

    [#7] fix: mdx style bug

--- src/components/mdx/AnchorText.tsx
+++ src/components/mdx/AnchorText.tsx
@@ -4,25 +4,17 @@ import styles from "./styles.module.scss"
 export type AnchorTextProps<C extends ElementType> = {
   as?: C
   children: ReactNode
-  href: string
 } & ComponentPropsWithoutRef<C>
-
-// anchor text
-/**
- *
- * @param prop
- * @returns
- */
 
 export default function AnchorText<C extends ElementType = "span">(
   props: AnchorTextProps<C>
 ) {
-  const { as = "span", children, href, className, ...rest } = props
+  const { as = "span", children, className, ...rest } = props
   const Component = as
   const cn = `${className} ${styles.anchor}`
   return (
-    <Component {...rest} id={href} className={cn}>
-      <a href={`#${href}`}>{children}</a>
+    <Component {...rest} id={children as string} className={cn}>
+      <a href={`#${children}`}>{children}</a>
     </Component>
   )
 }
