099e14973aa9e9e7d58ab07f62a132b234789b6a
sanam <simian114@stunning.kr>
2023-04-30 00:04:08 +0900

    [#58] chg: polymorphic props

--- src/components/polymorphic/Polymorphic.tsx
+++ src/components/polymorphic/Polymorphic.tsx
@@ -1,11 +1,19 @@
-import { ComponentPropsWithoutRef, ElementType } from "react"
+import { ElementType } from "react"
 
-export type PolymorphicProps<C extends ElementType> = {
+type AsProp<C extends React.ElementType> = {
   as?: C
-} & ComponentPropsWithoutRef<C>
+}
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P)
+
+export type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = object
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>
 
 export const Polymorphic = <C extends ElementType>(
-  props: PolymorphicProps<C>
+  props: PolymorphicComponentProp<C>
 ) => {
   const { as, ...rest } = props
   const Component = as || "span"
