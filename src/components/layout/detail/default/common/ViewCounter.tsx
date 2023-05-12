a7b84d6c607653069e59949001a40048f6d0f080
sanam <simian114@stunning.kr>
2023-05-13 00:11:52 +0900

    [#75] add: view count

---
+++ src/components/layout/detail/default/common/ViewCounter.tsx
@@ -0,0 +1,41 @@
+"use client"
+import useSWR from "swr"
+import { ReactElement } from "react"
+import { EyeOpenIcon } from "@radix-ui/react-icons"
+
+const fetcher = (slugs: string) =>
+  fetch(`${process.env.NEXT_PUBLIC_KV_REST_API_URL}/incr`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_KV_REST_API_TOKEN}`,
+    },
+    body: slugs,
+    method: "POST",
+  })
+    .then(res => res.json())
+    .then(data => data.result)
+
+interface UsePostViewCountProps {
+  slug: string[]
+}
+
+function usePostViewCounter(props: UsePostViewCountProps) {
+  return useSWR(`${props.slug.join("/")}`, () => fetcher(props.slug.join("/")))
+}
+
+interface ViewCounterProps {
+  slug: string[]
+}
+
+export default function ViewCounter(props: ViewCounterProps): ReactElement {
+  const { data, isLoading } = usePostViewCounter({ slug: props.slug })
+
+  return (
+    <div className={`detail-main__view-count `}>
+      <EyeOpenIcon />
+
+      <span className={`${isLoading ? "skeleton skeleton-text" : ""}`}>
+        {!isLoading && (data || 0)}
+      </span>
+    </div>
+  )
+}
