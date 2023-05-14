36d592cf74258f9bc15b695e10a29cc286eb7d59
sanam <simian114@stunning.kr>
2023-05-15 01:18:13 +0900

    [#94] add: comments using giscus

--- src/components/layout/detail/default/_default.tsx
+++ src/components/layout/detail/default/_default.tsx
@@ -1,3 +1,4 @@
+import Comments from "@/components/comment/Comments"
 import { allPulishedPost } from "@/lib/server"
 import { CalendarIcon, LapTimerIcon } from "@radix-ui/react-icons"
 import dayjs from "dayjs"
@@ -22,47 +23,50 @@ export default function DetailDefaultLayout({
   }
 
   return (
-    <div className="detail-main-wrapper">
-      <main className="detail-main">
-        <div className="detail-main__title-wrapper">
-          <h1 className="detail-main__title">{post.title}</h1>
-          <div className="detail-main__info">
-            <div className="detail-main__info__left">
-              <div className="detail-main__date">
-                <CalendarIcon />
-                <span>{dayjs(post.date).format("YYYY-MM-DD")}</span>
+    <>
+      <div className="detail-main-wrapper">
+        <main className="detail-main">
+          <div className="detail-main__title-wrapper">
+            <h1 className="detail-main__title">{post.title}</h1>
+            <div className="detail-main__info">
+              <div className="detail-main__info__left">
+                <div className="detail-main__date">
+                  <CalendarIcon />
+                  <span>{dayjs(post.date).format("YYYY-MM-DD")}</span>
+                </div>
+                <div className="detail-main__reading-time">
+                  <LapTimerIcon />
+                  <span>{post.readingTime}min</span>
+                </div>
+                <ViewCounter slug={slug} />
               </div>
-              <div className="detail-main__reading-time">
-                <LapTimerIcon />
-                <span>{post.readingTime}min</span>
+              <div className="detail-main__info__right">
+                <div className="detail-main__tags">
+                  {!!post.tags?.length && (
+                    <div className="post-card__content__tag-container">
+                      {post.tags.map(tag => (
+                        <Tag colorType={tag.colorType} key={tag.content}>
+                          {tag.content}
+                        </Tag>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </div>
-              <ViewCounter slug={slug} />
-            </div>
-            <div className="detail-main__info__right">
-              <div className="detail-main__tags">
-                {!!post.tags?.length && (
-                  <div className="post-card__content__tag-container">
-                    {post.tags.map(tag => (
-                      <Tag colorType={tag.colorType} key={tag.content}>
-                        {tag.content}
-                      </Tag>
-                    ))}
-                  </div>
-                )}
-              </div>
-            </div>
 
-            {/*  */}
+              {/*  */}
+            </div>
           </div>
-        </div>
-        <div className="detail-main__contents">{children}</div>
-      </main>
-      <aside className="sidebar-container">
-        <nav>
-          <DocumentTOC headings={post.headings} />
-        </nav>
-      </aside>
-      <MobileController headings={post.headings} />
-    </div>
+          <div className="detail-main__contents">{children}</div>
+        </main>
+        <aside className="sidebar-container">
+          <nav>
+            <DocumentTOC headings={post.headings} />
+          </nav>
+        </aside>
+        <MobileController headings={post.headings} />
+      </div>
+      <Comments />
+    </>
   )
 }
