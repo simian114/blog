d43047276269f774a33176f55e4ba9207b2e31b3
sanam <simian114@stunning.kr>
2023-05-17 01:06:23 +0900

    [#106] chg: snippet page

--- src/app/snippet/page.tsx
+++ src/app/snippet/page.tsx
@@ -1,11 +1,8 @@
-import CategoryList from "@/components/layout/index/default/common/CategoryList"
-import PostCard from "@/components/postCard/PostCard"
+import Tag from "@/components/postCard/tag/Tag"
+import Typography from "@/components/typography/Typography"
 import { getPostBySlugs } from "@/helpers/slug"
-import {
-  allSnippetPosts,
-  snippetPostsByCategory,
-} from "@/lib/server/post.server"
-import { allPosts } from "contentlayer/generated"
+import { allSnippetPosts } from "@/lib/server/post.server"
+import Link from "next/link"
 import { MdxContent } from "../mdx-content"
 
 export default async function Snippet() {
@@ -13,23 +10,62 @@ export default async function Snippet() {
   const post = await getPostBySlugs("/snippet")
 
   return (
-    <main className="index-main">
-      <section className="index-main__mdx-wrapper">
+    <main className="snippet-main">
+      <section className="snippet-main__mdx-wrapper">
         <MdxContent post={post} />
       </section>
-      <CategoryList
-        page="snippet"
-        categoryPosts={snippetPostsByCategory}
-        className="index-main__category-list"
-        allPosts={allPosts}
-      />
-      <section className="index-main__card-list">
-        <h2>최신 글</h2>
-        <div className="post-container">
-          {posts.map(post => (
-            <PostCard key={post._id} post={post} />
-          ))}
-        </div>
+      <section>
+        <table className="snippet-main__table snippet-table">
+          <thead className="snippet-table__thead">
+            <tr className="snippet-table__tr snippet-table__tr--head">
+              <th className="snippet-table__th snippet-table__th--name">
+                <Typography weight="bold">NAME</Typography>
+              </th>
+              <th className="snippet-table__th snippet-table__th--description">
+                <Typography weight="bold">DESCRIPTION</Typography>
+              </th>
+              <th className="snippet-table__th snippet-table__th--category">
+                <Typography weight="bold">CATEGORY</Typography>
+              </th>
+              <th className="snippet-table__th snippet-table__th--tags">
+                <Typography weight="bold">TAGS</Typography>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="snippet-table__tbody">
+            {posts.map(post => (
+              <tr
+                key={post._id}
+                className="snippet-table__tr snippet-table__tr--item"
+              >
+                <td className="snippet-table__td snippet-table__td--name">
+                  <Link href={post.slug}>
+                    <Typography
+                      colorLevel={10}
+                      colorType="primary"
+                      weight="bold"
+                    >
+                      {post.title}
+                    </Typography>
+                  </Link>
+                </td>
+                <td className="snippet-table__td snippet-table__td--description">
+                  <Typography>{post.description}</Typography>
+                </td>
+                <td className="snippet-table__td snippet-table__td--category">
+                  <Typography>{post.category}</Typography>
+                </td>
+                <td className="snippet-table__td snippet-table__td--tags">
+                  {post.tags?.map((tag, i) => (
+                    <Tag key={i} colorType={tag.colorType}>
+                      {tag.content}
+                    </Tag>
+                  ))}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
     </main>
   )
