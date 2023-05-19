23cc2a3c162f7795eb51eb266ea8de58ae99726a
sanam <simian114@stunning.kr>
2023-05-20 02:21:29 +0900

    [#105] add: dropdown to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,12 @@
 "use client"
 import Link from "next/link"
+import { ReactElement, useEffect, useMemo, useRef, useState } from "react"
 import {
-  ReactElement,
-  ReactNode,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react"
-import { RocketIcon } from "@radix-ui/react-icons"
+  ArrowDownIcon,
+  CaretDownIcon,
+  CaretUpIcon,
+  RocketIcon,
+} from "@radix-ui/react-icons"
 import ThemeToggler from "../theme/themeToggler"
 
 // NOTE: clinet-side only
@@ -16,32 +14,37 @@ import { usePathname } from "next/navigation"
 import { useDevice } from "@/store/deviceWidthProvider"
 import DisableScroll from "../client/DisableScroll"
 import ButtonLink from "../button/ButtonLink"
-
-interface Menu {
-  id: string
-  href: string
-  children: ReactNode
+import { Menu } from "@/app/layout"
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+interface HeaderProps {
+  menus: Menu[]
 }
 
-const menus: Menu[] = [
-  { id: "blog", children: "blog", href: "/blog" },
-  { id: "snippet", children: "snippet", href: "/snippet" },
-  { id: "archives", children: "archives", href: "/archives" },
-  { id: "mdx", children: "mdx", href: "/mdx" },
-]
-
-export default function Nav(): ReactElement {
+// NOTE: pc / mobile 분리하기
+export default function Nav(props: HeaderProps): ReactElement {
   const pathname = usePathname()
   const headerRef = useRef<HTMLHeadElement>(null)
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const { isMobile } = useDevice()
+
   const open = useMemo(
     () => isMobile && isMobileMenuOpen,
     [isMobile, isMobileMenuOpen]
   )
 
+  const [temp, setTemp] = useState(() => {
+    return props.menus.reduce((prev, cur) => {
+      if (!cur.categories?.length) return prev
+      return { ...prev, [cur.id]: false }
+    }, {} as { [key: string]: boolean })
+  })
+
   function toggleMobileMeun() {
     setIsMobileMenuOpen(prev => !prev)
+  }
+
+  function handleOpenChange({ route, open }: { route: string; open: boolean }) {
+    setTemp({ ...temp, [route]: open })
   }
 
   useEffect(() => {
@@ -64,18 +67,61 @@ export default function Nav(): ReactElement {
             className="navigation__menus"
             data-state={open ? "open" : "close"}
           >
-            {menus.map(menu => (
-              <li
-                key={menu.id}
-                className={
-                  pathname !== "/" && pathname.startsWith(menu.href)
-                    ? "active"
-                    : ""
-                }
-              >
-                <Link href={menu.href} key={menu.id}>
-                  {menu.children}
-                </Link>
+            {props.menus.map(menu => (
+              <li key={menu.id} className={`navigation__menu-item`}>
+                <DropdownMenu.Root
+                  onOpenChange={open =>
+                    handleOpenChange({ route: menu.id, open })
+                  }
+                >
+                  <Link
+                    className={`navigation__menu-link ${
+                      pathname !== "/" && pathname.startsWith(menu.href)
+                        ? "active"
+                        : ""
+                    }`}
+                    href={menu.href}
+                    key={menu.id}
+                  >
+                    {menu.children}
+                  </Link>
+                  {!isMobile && !!menu.categories?.length && (
+                    <>
+                      <DropdownMenu.Trigger asChild>
+                        <button className="navigation__icon-wrapper">
+                          <CaretDownIcon
+                            className={`navigation__icon navigation__icon--${
+                              temp[menu.id] ? "open" : "close"
+                            }`}
+                            width={16}
+                            height={16}
+                          />
+                        </button>
+                      </DropdownMenu.Trigger>
+                      <DropdownMenu.Portal>
+                        <DropdownMenu.Content
+                          className="navigation__content"
+                          asChild
+                        >
+                          <ul>
+                            {menu.categories.map(item => (
+                              <DropdownMenu.Item
+                                className="navigation__content-item"
+                                asChild
+                                key={item}
+                              >
+                                <Link href={`/${menu.id}?category=${item}`}>
+                                  {item}
+                                </Link>
+                              </DropdownMenu.Item>
+                            ))}
+                            <DropdownMenu.Arrow className="navigation__arrow DropdownMenuArrow" />
+                          </ul>
+                        </DropdownMenu.Content>
+                      </DropdownMenu.Portal>
+                    </>
+                  )}
+                </DropdownMenu.Root>
               </li>
             ))}
           </ul>
