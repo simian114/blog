ab1cb930457335203120c14a2eab0f8ed3e95a3b
sanam <simian114@stunning.kr>
2023-05-27 14:03:06 +0900

    [#129] del: useless await keyword

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -11,7 +11,7 @@ export async function generateStaticParams() {
 // contentlayer 로 모든 데이터 파싱
 export default async function BlogDetailndex({ params }: { params: any }) {
   const { slug } = params
-  const post = await getPostBySlugs("/blog", slug)
+  const post = getPostBySlugs("/blog", slug)
 
   return <MdxContent post={post} />
 }
