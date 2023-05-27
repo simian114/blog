0f94db5b745d78716c5f074febf501be23fbfa45
sanam <simian114@stunning.kr>
2023-05-27 15:12:40 +0900

    [#130] refactor: apply design system to footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,15 +1,17 @@
-import Link from "next/link"
 import { GitHubLogoIcon, RocketIcon } from "@radix-ui/react-icons"
+
+import IconButtonLink from "../button/IconButtonLink"
+import Typography from "../typography/Typography"
 
 const LINKS = [
   {
     href: "https://github.com/simian114",
-    children: <GitHubLogoIcon width={20} height={20} />,
+    children: <GitHubLogoIcon width={20} height={20} color="white" />,
     target: "_blank",
   },
   {
     href: "https://recketman.vercel.app",
-    children: <RocketIcon width={20} height={20} />,
+    children: <RocketIcon width={20} height={20} color="white" />,
     target: "_blank",
   },
 ]
@@ -19,16 +21,28 @@ export default function Footer() {
     <footer className="footer inner">
       <div className="footer__spacer" />
       <div className="footer__contents">
-        <div className="footer__contents__links">
-          {LINKS.map(link => (
-            <Link key={link.href} {...link} />
-          ))}
+        <div className="footer__link-container">
+          <>
+            {LINKS.map(link => (
+              <IconButtonLink
+                design={{ size: "small" }}
+                key={link.href}
+                target={link.target}
+                href={link.href}
+                className="footer__link"
+              >
+                {link.children}
+              </IconButtonLink>
+            ))}
+          </>
         </div>
-        <div className="footer__contents__text">
-          <span>
-            Developed By <strong>Rocketman</strong>
-          </span>
-        </div>
+        <Typography as="p" variants="body1">
+          Developed By
+          <Typography as="strong" variants="body1" weight="bold">
+            {" "}
+            Rocketman
+          </Typography>
+        </Typography>
       </div>
     </footer>
   )
