be6def369d15af328682ff55227997836631a91b
sanam <simian114@stunning.kr>
2023-06-02 01:13:31 +0900

    [#135] refactor: useFormStatus

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,40 +1,28 @@
-import { PrismaClient } from "@prisma/client"
+import { cache } from "react"
+import { guestbooks } from "@prisma/client"
 
 import Typography from "@/components/typography/Typography"
 
-import { createGuestBook } from "./actions"
-import Form from "./form"
-
-async function getData() {
-  const prisma = new PrismaClient()
-  const books = await prisma.guestbooks.findMany({
-    orderBy: { createdAt: "desc" },
-  })
-
-  return books
-}
+const getData = cache(async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/guestbook`)
+  return (await res.json()) as guestbooks[]
+})
 
 async function GuestBook() {
   const books = await getData()
   return (
-    <main className="guestbook-page">
-      <Typography variants="h1" as="h1" colorLevel={12}>
-        방명록
-      </Typography>
-      <Form createGuestBook={createGuestBook} />
-      <ul className="guestbook-page__comment-container">
-        {books.map((book, index) => (
-          <li key={index} className="guestbook-page__comment-wrapper">
-            <div className="guestbook-page__comment">
-              <Typography>{book?.nickname}: </Typography>
-              <Typography weight="medium" colorLevel={12}>
-                {book.content}
-              </Typography>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <ul className="guestbook-page__comment-container">
+      {books.map((book, index) => (
+        <li key={index} className="guestbook-page__comment-wrapper">
+          <div className="guestbook-page__comment">
+            <Typography>{book?.nickname}: </Typography>
+            <Typography weight="medium" colorLevel={12}>
+              {book.content}
+            </Typography>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
 
---
+++ src/app/guestbook/submitButton.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { experimental_useFormStatus as useFormState } from "react-dom"
+
+// import { experimental_useFormStatus as useFormStatus } from "react-dom"
+import Button from "@/components/button/Button"
+import { useDevice } from "@/store/deviceWidthProvider"
+
+export function SubmitButton() {
+  const { isMobile } = useDevice()
+  const { pending } = useFormState()
+
+  return (
+    <Button
+      type="submit"
+      design={{ size: "large" }}
+      baseDesign={{ fluid: isMobile }}
+      disabled={pending}
+      style={{ justifyContent: "center" }}
+    >
+      {pending ? "로딩" : "전송"}
+    </Button>
+  )
+}
