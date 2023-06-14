f98a7b08644a812f1f364592320494cf6f5146e2
sanam <simian114@stunning.kr>
2023-06-15 00:37:40 +0900

    [#143] chg: guestbook as api

--- src/app/api/guestbook/route.ts
+++ src/app/api/guestbook/route.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   const books = await prisma.guestbooks.findMany({
     orderBy: { createdAt: "desc" },
