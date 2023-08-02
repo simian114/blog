fe6de570a3f747e7b638d55da5dfcc6baaff12cb
sanam <simian114@stunning.kr>
2023-08-02 23:21:06 +0900

    refactor: postlist to PostList and CategorySelector

---
+++ src/components/layout/index/default/common/categorySelector/CategorySelector.tsx
@@ -0,0 +1,92 @@
+import Link from "next/link"
+import { Category, Route } from "@prisma/client"
+
+import Typography from "@/components/typography/Typography"
+import prisma from "@/lib/prisma"
+
+// NOTE: server component 기 때문에 category 를 넘겨 줄 필요가 없음
+
+interface CategorySelectorProps {
+  currentCategory?: Category
+  currentRoute: Route
+}
+
+async function getCategories({ routeId }: { routeId: number }) {
+  const categories = await prisma.category.findMany({
+    where: {
+      routeId,
+    },
+    include: {
+      route: true,
+      posts: {
+        include: { tags: { include: { tag: true } } },
+      },
+    },
+  })
+  return categories
+}
+
+export default async function CategorySelector(props: CategorySelectorProps) {
+  const categories = await getCategories({ routeId: props.currentRoute.id })
+  return (
+    <section className="post-list__category-section">
+      <ul className="post-list__category-list">
+        <li
+          className={`post-list__category-item ${
+            !props.currentCategory ? "post-list__category-item--active" : ""
+          }`}
+        >
+          <Link
+            scroll={false}
+            className={`post-list__link ${
+              !props.currentCategory ? "active" : ""
+            } ${!props.currentCategory ? "post-list__link--active" : ""}`}
+            href={`/${props.currentRoute.url}`}
+          >
+            <Typography
+              as="h3"
+              variants="h3"
+              colorType="gray"
+              colorLevel={11}
+              className="post-list__name"
+            >
+              전체
+            </Typography>
+          </Link>
+        </li>
+        {categories.map(category => (
+          <li
+            key={category.id}
+            className={`post-list__category-item ${
+              category.url === props.currentCategory?.url
+                ? "post-list__category-item--active"
+                : ""
+            }`}
+          >
+            <Link
+              scroll={false}
+              className={`post-list__link ${
+                category.url === props.currentCategory?.url ? "active" : ""
+              } ${
+                category.url === props.currentCategory?.url
+                  ? "post-list__link--active"
+                  : ""
+              }`}
+              href={`/${props.currentRoute.url}/${category.url}`}
+            >
+              <Typography
+                as="h3"
+                variants="h3"
+                colorType="gray"
+                colorLevel={11}
+                className="post-list__name"
+              >
+                {category.title}
+              </Typography>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
---
+++ src/components/layout/index/default/common/postList/PostList.tsx
@@ -0,0 +1,49 @@
+import { RouteLayoutType } from "@prisma/client"
+
+import Typography from "@/components/typography/Typography"
+import prisma from "@/lib/prisma"
+
+import { PostListMapper } from "./config"
+
+export interface PostListProp {
+  description: string
+  categoryId?: number
+  routeId: number
+  type: RouteLayoutType
+}
+
+async function getData({ routeId }: { routeId: number }) {
+  const posts = await prisma.post.findMany({
+    where: {
+      routeId,
+    },
+    include: {
+      route: true,
+      category: true,
+      tags: { include: { tag: true } },
+    },
+  })
+  return posts
+}
+
+export default async function PostList(props: PostListProp) {
+  const allPosts = await getData({ routeId: props.routeId })
+  const posts = allPosts.filter(post => post.categoryId === props.categoryId)
+  const List = PostListMapper[props.type]
+
+  return (
+    <section className="post-list__post-section">
+      {props.description && (
+        <Typography
+          as="h4"
+          variants="subtitle1"
+          colorType="gray"
+          colorLevel={12}
+        >
+          {props.description}
+        </Typography>
+      )}
+      {!!List && <List posts={props.categoryId ? posts : allPosts} />}
+    </section>
+  )
+}
