df87e6265c7cfa36e5e8a0abe293f8e24720d9b8
sanam <simian114@stunning.kr>
2023-08-18 00:29:01 +0900

    [#189] add: custom cumponents to mdx editor

--- src/app/(admin)/admin/posts/new/MdxEditorContainer.tsx
+++ src/app/(admin)/admin/posts/new/MdxEditorContainer.tsx
@@ -1,6 +1,32 @@
 "use client"
 import dynamic from "next/dynamic"
-import { useState } from "react"
+import React, { useState } from "react"
+import {
+  AdmonitionDirectiveDescriptor,
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  codeBlockPlugin,
+  CodeToggle,
+  CreateLink,
+  diffSourcePlugin,
+  DiffSourceToggleWrapper,
+  directivesPlugin,
+  GenericJsxEditor,
+  headingsPlugin,
+  InsertAdmonition,
+  InsertCodeBlock,
+  JsxComponentDescriptor,
+  jsxPlugin,
+  jsxPluginHooks,
+  linkPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  MDXEditorMethods,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo,
+} from "@mdxeditor/editor"
 import { Tag } from "@prisma/client"
 
 import Button from "@/components/button/Button"
@@ -21,7 +47,37 @@ interface MdxEditorContainerProps {
 }
 
 export default function MdxEditorContainer(props: MdxEditorContainerProps) {
-  const [markdown, setMarkdown] = useState("# hello world!")
+  const [markdown, setMarkdown] = useState(`# hello world\n # hwy?`)
+  const ref = React.useRef<MDXEditorMethods>(null)
+
+  // NOTE: 공백 컴포넌트
+  const jsxComponentDescriptors: JsxComponentDescriptor[] = [
+    {
+      name: "br",
+      kind: "flow",
+      source: "source is not important",
+      defaultExport: true,
+      props: [{ name: "foo", type: "string" }],
+      Editor: GenericJsxEditor,
+    },
+  ]
+
+  const InsertBR = () => {
+    const insertJsx = jsxPluginHooks.usePublisher("insertJsx")
+    return (
+      <Button
+        onClick={() =>
+          insertJsx({
+            name: "br",
+            kind: "flow",
+            props: { foo: "bar", bar: "baz" },
+          })
+        }
+      >
+        InsertBR
+      </Button>
+    )
+  }
 
   return (
     <div className="flex flex-col gap-8">
@@ -38,9 +94,45 @@ export default function MdxEditorContainer(props: MdxEditorContainerProps) {
 
       <div className="border border-solid rounded h-full">
         <MDXEditor
+          ref={ref}
           markdown={markdown}
           onChange={setMarkdown}
           contentEditableClassName="mdx"
+          plugins={[
+            jsxPlugin({ jsxComponentDescriptors }),
+            listsPlugin(),
+            directivesPlugin({
+              directiveDescriptors: [AdmonitionDirectiveDescriptor],
+            }),
+            linkPlugin(),
+            listsPlugin(),
+            headingsPlugin(),
+            codeBlockPlugin(),
+            quotePlugin(),
+            markdownShortcutPlugin(),
+            thematicBreakPlugin(),
+            diffSourcePlugin({
+              diffMarkdown: "An older version",
+              viewMode: "rich-text",
+            }),
+            toolbarPlugin({
+              toolbarContents: () => (
+                <>
+                  <DiffSourceToggleWrapper>
+                    <UndoRedo />
+                  </DiffSourceToggleWrapper>
+                  <UndoRedo />
+                  <BlockTypeSelect />
+                  <BoldItalicUnderlineToggles />
+                  <CodeToggle />
+                  <CreateLink />
+                  <InsertAdmonition />
+                  <InsertCodeBlock />
+                  <InsertBR />
+                </>
+              ),
+            }),
+          ]}
         />
       </div>
     </div>
