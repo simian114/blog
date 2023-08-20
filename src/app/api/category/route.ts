322e222a2cc23ec6e246b9f802113250636ad219
sanam <simian114@stunning.kr>
2023-08-20 22:43:50 +0900

    [#173] chg: route as dynamic

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -2,6 +2,8 @@
 
 import prisma from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   const allCategories = await prisma.category.findMany({
     include: { route: true, posts: true },
--- src/app/api/route/route.ts
+++ src/app/api/route/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   const routes = await prisma.route.findMany({
     include: { categories: true, posts: true, layouts: true },
