520c14d246e73da71dd717f1cbd523870e60977e
sanam <simian114@stunning.kr>
2023-08-20 22:43:50 +0900

    [#173] chg: route as dynamic

--- src/app/api/tag/route.ts
+++ src/app/api/tag/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   // const allCategories = await prisma.tags.findMany({})
   const tags = await prisma.tag.findMany()
