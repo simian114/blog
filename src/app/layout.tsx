9a32ce1ba6a3a8d78666c49a78f3eede1cc1ae8c
sanam <simian114@stunning.kr>
2023-08-27 13:43:52 +0900

    [#199] add: sitemap

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return <>{children}</>
+}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,70 @@
 import { MetadataRoute } from "next"
+import { ComponentType } from "@prisma/client"
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return []
-  // return allPublishedPost.map(post => ({
-  //   url: `${process.env.URL}${post.url.replace("index", "")}`,
-  //   lastModified: post.publishedAt,
-  // }))
+import { getPostURL } from "@/helpers/model/post"
+import prisma from "@/lib/prisma"
+
+// NOTE: last modified 가 있어야함.
+// tag 경로
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routes = await prisma.route.findMany({
+    where: { open: true, deletedAt: null },
+    include: { categories: true, components: true },
+  })
+  const routeMap = routes.map(route => ({
+    url: `/${route.url}`,
+    lastModified: route.updatedAt,
+  }))
+
+  const categoryMap = routes.reduce((prev, cur) => {
+    if (!cur.categories.length) {
+      return prev
+    }
+    const routeCategoryMap = cur.categories.map(category => ({
+      url: `/${cur.url}/${category.url}`,
+      lastModified: category.updatedAt.toISOString(),
+    }))
+    return [...prev, ...routeCategoryMap]
+  }, [] as Array<{ url: string; lastModified: string }>)
+
+  const posts = await prisma.post.findMany({
+    where: { deletedAt: null, published: true },
+    include: { route: true, category: true, tags: { include: { tag: true } } },
+  })
+  const postMap = posts.map(post => ({
+    url: getPostURL(post),
+    lastModified: post.updatedAt.toISOString(),
+  }))
+
+  const hasTagSelectorRoutes = routes.filter(route =>
+    route.components.find(
+      component =>
+        component.type === ComponentType.SUB_URL &&
+        component.name === "TagSelector"
+    )
+  )
+
+  const tagRouteMap = posts.reduce((prev, cur) => {
+    console.log(hasTagSelectorRoutes)
+    if (!cur.tags.length) {
+      return prev
+    }
+
+    const routeTag = hasTagSelectorRoutes
+      .map(route =>
+        cur.tags.map(tag => ({
+          url: `/${route.url}/${tag.tag.url}`,
+          lastModified: tag.tag.updatedAt.toISOString(),
+        }))
+      )
+      .flat()
+    return [...prev, ...routeTag]
+  }, [] as Array<{ url: string; lastModified: string }>)
+
+  return [...routeMap, ...categoryMap, ...postMap, ...tagRouteMap].map(
+    siteMapItem => ({
+      url: `${process.env.URL}${siteMapItem.url}`,
+      lastModified: siteMapItem.lastModified,
+    })
+  )
 }
