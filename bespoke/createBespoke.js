b0dd0123b6192f2bd91f257c2c4d622192992308
sanam <simian114@stunning.kr>
2023-09-02 01:38:30 +0900

    [#197] refactor: bespoke system build process

--- bespoke/createBespoke.js
+++ bespoke/createBespoke.js
@@ -1,23 +1,24 @@
+// import dox from "dox"
+
 /* eslint-disable */
-const fs = require("fs").promises
-const path = require("path")
+const fs = require("fs").promises;
+const path = require("path");
+const { parse } = require("comment-parser");
 
-const createColor = require("./createColor.js")
-const createComponents = require("./createComponents.js")
+const createColor = require("./createColor.js");
+const createComponents = require("./createComponents.js");
 
-module.exports =
-  (pluginOptions = {}) =>
-  async (nextConfig = {}) => {
-    await createColor(pluginOptions?.colors)
-    await createComponents()
+module.exports = (pluginOptions = {}) => async (nextConfig = {}) => {
+  await createColor(pluginOptions?.colors);
+  await createComponents();
 
-    return Object.assign({}, nextConfig, {
-      webpack(config, options) {
-        if (typeof nextConfig.webpack === "function") {
-          return nextConfig.webpack(config, options)
-        }
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
 
-        return config
-      },
-    })
-  }
+      return config;
+    },
+  });
+};
--- bespoke/createComponents.js
+++ bespoke/createComponents.js
@@ -1,32 +1,49 @@
 /* eslint-disable */
-const fs = require("fs").promises
-const path = require("path")
+const fs = require("fs").promises;
+const path = require("path");
+const { parse } = require("comment-parser");
 
-const ROOT = path.resolve()
-const TARGET = ROOT + "/src/constants/bespoke-components.ts"
+// NOTE: eslint 로 파일 바로formating 할 수 있지 않을까?
+const { ESLint } = require("eslint");
 
-const LAYOUT_COMPONENTS_PATH = ROOT + "/src/components/layout/components"
+const ROOT = path.resolve();
+const TARGET = ROOT + "/src/constants/bespoke-components.ts";
+const LAYOUT_COMPONENTS_PATH = ROOT + "/src/components/layout/components";
+
+// const eslint = new ESLint({ fix: true });
+const eslint = new ESLint({ fix: true, useEslintrc: true });
 
 async function createComponents() {
-  const filesWithExt = await fs.readdir(LAYOUT_COMPONENTS_PATH)
-  const files = filesWithExt
-    .map(file => file.split(".")[0])
-    .filter(file => file !== "index")
-  const template = getComponentTemplate(files)
-  await fs.writeFile(TARGET, template)
-}
+  const filesWithExt = await fs.readdir(LAYOUT_COMPONENTS_PATH);
+  const r = {};
 
-module.exports = createComponents
+  // TODO: console.log 로 어떤 컴포넌트가 만들어지는지 표현해주기
+  for await (const fileWithExt of filesWithExt) {
+    if (fileWithExt === "index.ts") {
+      continue;
+    }
+    const fileAsString = await fs.readFile(
+      `${LAYOUT_COMPONENTS_PATH}/${fileWithExt}`,
+      { encoding: "utf-8" },
+    );
+    const parsedJSDoc = parse(fileAsString, { spacing: "preserve" });
 
-function getComponentTemplate(components) {
-  if (!Array.isArray(components)) {
-    return `const ComponentList = ${JSON.stringify([])}
+    const props = parsedJSDoc[0]?.tags.reduce((prev, tag) => {
+      if (tag.tag !== "param" || !tag.name || !tag.type) {
+        return prev;
+      }
+      const { name, source, problems, param, ...rest } = tag;
+      prev[name] = { ...rest };
+      return prev;
+    }, {});
 
-export default ComponentList
-`
+    const fileName = fileWithExt.split(".")[0];
+    r[fileName] = props;
   }
-  return `const ComponentList = ${JSON.stringify(components)}
-
-export default ComponentList
-`
+  const beforeFormat = `export const ComponentProps = ${JSON.stringify(r)}`;
+  const afterFormat = (await eslint.lintText(beforeFormat))?.[0]?.output || "";
+  await fs.writeFile(TARGET, afterFormat);
 }
+
+module.exports = createComponents;
+
