cd46942a8d3fe46434f124c1337114926070f1c6
sanam <simian114@stunning.kr>
2023-09-03 14:12:56 +0900

    [#214] del: unused file

--- src/app/api/layout/component/route.ts
+++
@@ -1,8 +0,0 @@
-import getLayoutComponentList from "@/helpers/layout/getLayoutComponentList"
-
-export const dynamic = "force-dynamic"
-
-export async function GET() {
-  const files = await getLayoutComponentList()
-  return new Response(JSON.stringify(files))
-}
