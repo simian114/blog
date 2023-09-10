37536eb900c509721ecfb86cafc431027c8e780c
sanam <simian114@stunning.kr>
2023-09-10 19:04:57 +0900

    [#206] add: admin route group

--- src/app/(admin)/admin/(normal)/layout.tsx
+++ src/app/(admin)/admin/(normal)/layout.tsx
@@ -1,10 +1,22 @@
+import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth"
 import { ReactNode } from "react"
 
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { DocsSidebarNav } from "@/components/admin/SidebarNav"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { adminConfig } from "@/config/admin"
 
-export default function DocsLayout({ children }: { children: ReactNode }) {
+export default async function AdminLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect("/api/auth/signin")
+  }
+
   return (
     <div className="border-b">
       <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
