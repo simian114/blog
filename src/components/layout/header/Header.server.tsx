894ab7bc66d6cb6194df39750527f37569044fc7
sanam <simian114@stunning.kr>
2023-09-26 22:28:17 +0900

    [#233] refactor: header has get data responsibility

---
+++ src/components/layout/header/Header.server.tsx
@@ -0,0 +1,19 @@
+import HeaderClient from "./Header.client"
+
+async function getData() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/layout/header`,
+      { next: { tags: ["/api/layout/header"] } }
+    )
+    const routes = await res.json()
+    return { routes }
+  } catch (error) {
+    return { routes: [] }
+  }
+}
+
+export default async function Header() {
+  const { routes } = await getData()
+  return <HeaderClient routes={routes} />
+}
