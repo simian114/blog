d2dd0e4ddcbbf28753866a4bbe68040cb03f4960
sanam <simian114@stunning.kr>
2023-10-14 01:53:53 +0900

    [#249] chg: codeblock style

--- src/app/(main)/(custom)/mdx/page.tsx
+++ src/app/(main)/(custom)/mdx/page.tsx
@@ -1,20 +1,26 @@
 import { compileMDX } from "next-mdx-remote/rsc"
 import fs from "fs/promises"
 import path from "path"
-import rehypeHighlight from "rehype-highlight"
+import rehypePrettyCode from "rehype-pretty-code"
 import remarkGfm from "remark-gfm"
 
 import { MdxComponents } from "@/components/mdx/mdxComponents"
 
 export default async function MdxStyle() {
   const post = await fs.readFile(`${path.resolve()}/src/posts/mdx/index.mdx`)
+  const options = {
+    theme: {
+      light: "github-light",
+      dark: "one-dark-pro",
+    },
+  }
 
   const { content } = await compileMDX({
     source: post || "",
     options: {
       mdxOptions: {
         remarkPlugins: [remarkGfm],
-        rehypePlugins: [rehypeHighlight],
+        rehypePlugins: [() => rehypePrettyCode(options)],
         format: "mdx",
       },
     },
