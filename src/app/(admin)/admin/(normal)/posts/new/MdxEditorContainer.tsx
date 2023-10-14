1ed272ef320e0ef525eb7380ed4ff470040fc904
sanam <simian114@stunning.kr>
2023-10-14 18:40:39 +0900

    [#249] chg: mdxeditor codeblock meta

--- src/app/(admin)/admin/(normal)/posts/new/MdxEditorContainer.tsx
+++ src/app/(admin)/admin/(normal)/posts/new/MdxEditorContainer.tsx
@@ -5,6 +5,7 @@ import {
   AdmonitionDirectiveDescriptor,
   BlockTypeSelect,
   BoldItalicUnderlineToggles,
+  CodeBlockEditorDescriptor,
   codeBlockPlugin,
   codeMirrorPlugin,
   CodeToggle,
@@ -24,6 +25,7 @@ import {
   thematicBreakPlugin,
   toolbarPlugin,
   UndoRedo,
+  useCodeBlockEditorContext,
 } from "@mdxeditor/editor"
 
 import { AllIncludedPost } from "../[id]/page"
@@ -48,6 +50,35 @@ export default function MdxEditorContainer(props: MdxEditorContainerProps) {
   const [markdown, setMarkdown] = useState(
     props.post?.content || `# hello world\n # hwy?`
   )
+  // NOTE: meta 가 들어가면 터짐. 터지지 않기 위해 이렇게 하면 스타일이 없게됨. 해결할것
+  const PlainTextCodeEditorDescriptor: CodeBlockEditorDescriptor = {
+    // always use the editor, no matter the language or the meta of the code block
+    match: (language, meta) => {
+      console.log({ meta })
+      console.log({ language })
+      return true
+    },
+    priority: 0,
+    Editor: props => {
+      const cb = useCodeBlockEditorContext()
+      return (
+        <>
+          <div
+            onKeyDown={e => e.nativeEvent.stopImmediatePropagation()}
+            style={{ width: "100%" }}
+          >
+            <textarea
+              style={{ width: "100%" }}
+              rows={3}
+              cols={20}
+              defaultValue={props.code}
+              onChange={e => cb.setCode(e.target.value)}
+            />
+          </div>
+        </>
+      )
+    },
+  }
 
   return (
     <div className="flex flex-col gap-8">
@@ -70,7 +101,10 @@ export default function MdxEditorContainer(props: MdxEditorContainerProps) {
             linkDialogPlugin({}),
             listsPlugin(),
             headingsPlugin(),
-            codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+            codeBlockPlugin({
+              codeBlockEditorDescriptors: [PlainTextCodeEditorDescriptor],
+              defaultCodeBlockLanguage: "js",
+            }),
             codeMirrorPlugin({
               codeBlockLanguages: {
                 js: "JavaScript",
