3fa61f698c50b29fb8392c2e9d1ca75fed79a497
sanam <simian114@stunning.kr>
2023-11-27 23:56:04 +0900

    [#283] add: blog detail page head tag

--- src/app/(main)/(bespoke-detail)/[route]/[subURL]/[post]/page.tsx
+++ src/app/(main)/(bespoke-detail)/[route]/[subURL]/[post]/page.tsx
@@ -1,4 +1,9 @@
-import DetailDefaultLayout from "@/components/layout/detail/default/DetailDefaultLayout"
+import { Metadata } from "next"
+
+import DetailDefaultLayout, {
+  PostWithComponentRoute,
+} from "@/components/layout/detail/default/DetailDefaultLayout"
+import { defaultMeta } from "@/constants/metadata"
 import prisma from "@/lib/prisma"
 
 export const dynamicParams = true
@@ -15,6 +20,35 @@ export async function generateStaticParams() {
       subURL: post.category?.url || "",
       post: post.url,
     }))
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { route: string; subURL: string; post: string }
+}): Promise<Metadata> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/post/${params.post}`,
+      { next: { tags: [`/api/post/${params.post}`] } }
+    )
+    const post = (await res.json()) as PostWithComponentRoute
+    if (!post) {
+      return { ...defaultMeta }
+    }
+
+    return {
+      title: post.title,
+      description: post.description || "",
+      keywords: post.tags.map(tag => tag.tag.title),
+      openGraph: {
+        title: post.title,
+        description: post.description || "",
+      },
+    }
+  } catch (error) {
+    return { ...defaultMeta }
+  }
 }
 
 export default function DetailPage({
