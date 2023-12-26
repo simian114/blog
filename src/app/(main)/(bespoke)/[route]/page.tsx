232c583d24582242e08424a373c252ffe9c90029
sanam <simian114@stunning.kr>
2023-12-26 23:43:51 +0900

    [#289] chg: list page ssg to ssr

--- src/app/(main)/(bespoke)/[route]/page.tsx
+++ src/app/(main)/(bespoke)/[route]/page.tsx
@@ -1,28 +1,10 @@
 import { notFound } from "next/navigation"
-import { ROUTE_TYPE } from "@prisma/client"
-
-import prisma from "@/lib/prisma"
 
 import MainList from "./list"
 
 export const dynamicParams = true
 
-export async function generateStaticParams() {
-  const routes = (
-    await prisma.route.findMany({
-      where: { type: { equals: ROUTE_TYPE.BESPOKE } },
-      include: { components: true },
-    })
-  )
-    .filter(route => !!route.components.length)
-    .filter(route => route.url !== "")
-
-  const routeSlug = routes.map(route => ({
-    route: route.url,
-  }))
-
-  return routeSlug
-}
+export const revalidate = 3600
 
 export default async function RoutePage({
   params,
