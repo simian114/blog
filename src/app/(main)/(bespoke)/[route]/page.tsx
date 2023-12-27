58857869b791871e2af00cfe1f3f61018003281c
sanam <simian114@stunning.kr>
2023-12-27 14:05:28 +0900

    [#289] chg: revert to ssg

--- src/app/(main)/(bespoke)/[route]/page.tsx
+++ src/app/(main)/(bespoke)/[route]/page.tsx
@@ -1,10 +1,28 @@
 import { notFound } from "next/navigation"
+import { ROUTE_TYPE } from "@prisma/client"
+
+import prisma from "@/lib/prisma"
 
 import MainList from "./list"
 
 export const dynamicParams = true
 
-export const revalidate = 3600
+export async function generateStaticParams() {
+  const routes = (
+    await prisma.route.findMany({
+      where: { type: { equals: ROUTE_TYPE.BESPOKE } },
+      include: { components: true },
+    })
+  )
+    .filter(route => !!route.components.length)
+    .filter(route => route.url !== "")
+
+  const routeSlug = routes.map(route => ({
+    route: route.url,
+  }))
+
+  return routeSlug
+}
 
 export default async function RoutePage({
   params,
