bc6af00f92e7bec70a71b2ea6547b611909d3026
sanam <simian114@stunning.kr>
2024-01-05 22:59:22 +0900

    [#308] add: prevent motion in CSR

--- src/components/motion/Motion.tsx
+++ src/components/motion/Motion.tsx
@@ -4,14 +4,24 @@ import { motion, MotionProps } from "framer-motion"
 
 type MotionWrapperProps<C extends React.ElementType> = {
   as: C
+  preventAnimationInCSR?: boolean
 } & React.ComponentPropsWithoutRef<C> &
   MotionProps
 
 export default function Motion<C extends React.ElementType>(
   props: MotionWrapperProps<C>
 ) {
-  const { as, ...rest } = props
+  const { as, initial, preventAnimationInCSR, ...rest } = props
   const Component = motion[(as || "div") as HTMLElements]
 
-  return <Component {...rest} />
+  return (
+    <>
+      <Component
+        {...rest}
+        initial={
+          preventAnimationInCSR && typeof window === "undefined" ? {} : initial
+        }
+      />
+    </>
+  )
 }
