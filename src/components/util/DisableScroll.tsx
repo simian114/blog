268b2d4f176b871fc77052f05131261315cdc90a
sanam <simian114@stunning.kr>
2024-01-06 14:07:29 +0900

    [#311] chore: todo 주석

--- src/components/util/DisableScroll.tsx
+++ src/components/util/DisableScroll.tsx
@@ -4,6 +4,9 @@ type DisableScrollProps = {
   enable: boolean
 }
 
+/**
+ * TODO: 모달 나올 때 마다 스크롤바가 사라지면서 보기 안좋아짐. 스크롤바는 그대로 냅두면서 ..
+ */
 const DisableScroll = (props: DisableScrollProps): ReactElement => {
   useEffect(() => {
     if (props.enable) {
