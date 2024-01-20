0004df669aa082c9d2671fd1454c28122774c3bc
sanam <simian114@stunning.kr>
2024-01-20 11:28:10 +0900

    [#160] fix: suspense theme script
    suspense를 주지 않으면 로컬 개발환경에서 에러 발생.

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,12 +1,11 @@
 "use client"
 
-import { ReactElement, useState } from "react"
+import { ReactElement, Suspense, useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Provider as JotaiProvider } from "jotai"
 
 import DeviceWidthProvider from "@/components/providers/deviceWidthProvider"
-
-import ThemeScript from "../theme/ThemeScript"
+import ThemeScript from "@/components/theme/ThemeScript"
 
 interface ProvidersProps {
   children: JSX.Element[] | JSX.Element
@@ -16,7 +15,10 @@ export function Providers(props: ProvidersProps): ReactElement {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <JotaiProvider>
-      <ThemeScript />
+      {/* NOTE: 에러 1개 발생.. suspense로 감싸지 않는 경우에는 3개의 에러 발생함  */}
+      <Suspense>
+        <ThemeScript />
+      </Suspense>
       <DeviceWidthProvider>
         <QueryClientProvider client={queryClient}>
           {props.children}
--- src/components/theme/ThemeScript.tsx
+++ src/components/theme/ThemeScript.tsx
@@ -4,6 +4,11 @@ import { memo, useEffect } from "react"
 
 import { MEDIA_COLOR_SCHEME, Theme, useSetTheme } from "@/store/theme"
 
+/**
+ * 에러가 발생함.. 어떻게 하면 에러 발생하지 않게 만들 수 있을까?
+ * 이유: server에서 보내줄 때는 html / body에 theme이 없는데, html을 그리기 전에 여기서 theme값을 바로 넣어줌
+ * 따라서 서버에서 보내준 값과 실제 값이 달라서 hydration error가 발생
+ */
 const ThemeScript = memo(
   () => {
     const setTheme = useSetTheme()
